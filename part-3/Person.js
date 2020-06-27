e07948a66f33347505f036e942167415e2a54bf2
michael <michael@michaels-MBP.fios-router.home>
2020-06-26 20:39:34 -0400

    changed to loop with key value

--- part-3/Person.js
+++ part-3/Person.js
@@ -3,8 +3,8 @@ function Person(props) {
     <div>
       <p>Learn some information about this person</p>
       <ul>
-        {props.hobbies.map((value) => {
-          return <li>{value}</li>
+        {props.hobbies.map((value,index) => {
+          return <li key={index}>{value}</li>
         })}
       </ul>
       <h3>You {props.age < 18 ? "you must be 18" : "please go vote!"}</h3>
